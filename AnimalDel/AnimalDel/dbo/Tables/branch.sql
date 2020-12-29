de8e66b287af5985a468e70cd85944dea9796d49
admin <admin@MOUNTAIN>
2020-12-29 19:30:43 +0530

    ss

--- AnimalDel/AnimalDel/dbo/Tables/branch.sql
+++ AnimalDel/AnimalDel/dbo/Tables/branch.sql
@@ -2,7 +2,8 @@
     [dept_name] VARCHAR (15) NULL,
     [dept_id]   INT          NULL,
     [location]  VARCHAR (15) NULL,
-    [color]     VARCHAR (15) NULL
+    [color]     VARCHAR (15) NULL,
+    [name]     VARCHAR (15) NULL
 );
 
 
